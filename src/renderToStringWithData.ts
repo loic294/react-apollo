503b05decb24b89d81f68225f005462ff2239bc0
Loïc Bellemare-Alford <loicbellemare@hotmail.com>
2018-09-22 17:58:19 +0000

    Removed src

--- src/renderToStringWithData.ts
+++
@@ -1,8 +0,0 @@
-import { ReactElement } from 'react';
-import ReactDOM from 'react-dom/server';
-
-import { default as getDataFromTree } from './getDataFromTree';
-
-export function renderToStringWithData(component: ReactElement<any>): Promise<string> {
-  return getDataFromTree(component).then(() => ReactDOM.renderToString(component));
-}
